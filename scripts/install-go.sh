88f545ff05f82cd7bde8583a5bf73ba3beb8657f
Richard Chew <rchew@palantir.com>
2020-04-12 13:19:30 -0700

    Fix typo

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -8,6 +8,6 @@ OS=linux
 ARCH=armv6l
 VERSION=1.14.2
 
-wget https://dl.google.com/go/go${VERSIOM}.${OS}-${ARCH}.tar.gz
+wget "https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz"
 
-tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+tar -C /usr/local -xzf "go${VERSION}.${OS}-${ARCH}.tar.gz"
